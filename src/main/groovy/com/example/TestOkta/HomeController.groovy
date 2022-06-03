af92b885adeff99bbeaebbeb518de8932f50dfe7
Jovany Vergara <jovany@makingdevs.com>
2022-06-03 09:30:44 -0500

    [master] trying methods for the error I have

--- src/main/groovy/com/example/TestOkta/HomeController.groovy
+++ src/main/groovy/com/example/TestOkta/HomeController.groovy
@@ -1,5 +1,6 @@
 package com.example.TestOkta
 
+import org.apache.catalina.User
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.core.oidc.user.OidcUser
 import org.springframework.web.bind.annotation.GetMapping
@@ -9,7 +10,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HomeController {
     @GetMapping("/")
-    String getName (@AuthenticationPrincipal OidcUser user){
-        return "Login En: "+ user.getFamilyName() + "!"
+    String home(@AuthenticationPrincipal User user) {
+        return "Welcome, "+ user.getFullName() + "!";
+    }
+    @GetMapping("/helth")
+    String helth(@AuthenticationPrincipal User user) {
+        return "Welcome, "+ user.getFullName() + "!";
     }
 }
